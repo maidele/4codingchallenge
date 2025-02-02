19dac89965639b9b17a414201ee2e84a466794db
Maidele Archelus <maidele@usf.edu>
2025-02-02 16:43:53 -0500

    Task 1- Product Price Management

---
+++ cc_4.js
@@ -0,0 +1,13 @@
+//Task 1
+
+let purchaseAmount = 277; // Here a variable is being declared 
+
+if (purchaseAmount > 100) { // Recognizes whether the discount is more than $100
+        console.log("Discount has been applied!");  // Discount is displayed
+      } else {
+        console.log("Discount is not applicable");  // Discount is not displayed
+      }
+    console.log("Total value after the applied discount: $${finaltotalAmount}"); // Disount is applicable and displayed
+
+
+
